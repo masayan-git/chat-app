7bc283a84bbedde3d98419c254875e055765448f
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-07-28 15:58:22 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
